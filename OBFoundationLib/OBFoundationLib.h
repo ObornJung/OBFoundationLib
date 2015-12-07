77c9f0857d6c3b9aa003df6937a9bd156f097bf3
Oborn.Jung <obornjung@gmail.com>
2015-12-07 17:36:13 +0800

    Adjust OBFoundation file

--- OBFoundationLib/OBFoundationLib.h
+++ OBFoundationLib/OBFoundationLib.h
@@ -9,6 +9,14 @@
 #ifndef OBFoundationLib_h
 #define OBFoundationLib_h
 
+//! Project version number for OBFoundationLib.
+FOUNDATION_EXPORT double OBFoundationLibVersionNumber;
+
+//! Project version string for OBFoundationLib.
+FOUNDATION_EXPORT const unsigned char OBFoundationLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OBFoundationLib/PublicHeader.h>
+
 #import "OBMoudle.h"
 #import "OBToolsMacro.h"
 #import "OBSystemExtend.h"
